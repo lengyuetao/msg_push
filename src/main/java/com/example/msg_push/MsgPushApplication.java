7f345ad663e2ac7948931414ffed27e5176d8e7d
lengyuetao <dyadan@163.com>
2018-08-03 14:41:17 +0800

    消息推送

---
+++ src/main/java/com/example/msg_push/MsgPushApplication.java
@@ -0,0 +1,12 @@
+package com.example.msg_push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgPushApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/msg_push/MsgPushApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.msg_push;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsgPushApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
